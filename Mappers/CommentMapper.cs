0312a8dc906b2cf325bf2d30b03cce7ce7d68e3d
Beqa <beqa.basiladze1994@gmail.com>
2024-01-04 20:12:44 +0400

    add new files(Comment files)

---
+++ Mappers/CommentMapper.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.DTO.Comment;
+using api.Models;
+
+namespace api.Mappers
+{
+    public static class CommentMapper
+    {
+        public static CommentDTO ToCommentDto(this Comment commentModel)
+        {
+            return new CommentDTO
+            {
+                Id = commentModel.Id,
+                Title = commentModel.Title,
+                Content = commentModel.Content,
+                CreatedOn = commentModel.CreatedOn,
+                StockId = commentModel.StockId
+            };
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -17,6 +17,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 });
 
 builder.Services.AddScoped<IStockRepository, StockRepository>();
+builder.Services.AddScoped<ICommentRepository, CommentRepository>();
 
 var app = builder.Build();
 
